5befcff86b921d82717f5d8a1d02fed06cb6942c
Ramcharan-30 <ramcharankodisa.24@kgpian.iitkgp.ac.in>
2025-06-19 17:35:55 +0530

    Build Done

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,31 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/home'
-import Login from './pages/login'
-import Signup from './pages/signup'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
 import Modal from 'react-modal';
+
 Modal.setAppElement('#root');
 
-
-
-
-
-const routes=(
-  <Router>
-    <Routes>
-      <Route path="/dashboard" exact element={<Home />} />
-      <Route path="/Login" exact element={<Login />} />
-      <Route path="/Signup" exact element={<Signup />} />
-    </Routes>
-  </Router>
-)
-
-
 const App = () => {
-  return <>
-   {routes}
-  </>
-  
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<div className="text-center mt-10">404 - Page Not Found</div>} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
